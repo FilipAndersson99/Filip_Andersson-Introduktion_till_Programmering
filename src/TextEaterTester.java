4a3f7154d9251b1cba73d22ada5a30b90fd36eb5
FilipAndersson99 <andersson.filip.1999@gmail.com>
2021-10-29 14:47:38 +0200

    Testet testCharacterCounter är färdigt

--- src/TextEaterTester.java
+++ src/TextEaterTester.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TextEaterTester {
     @Test
     public void testCharacterCounter() {
         //Arrange
         TextEater text = new TextEater();
         //Act
+        int actual = text.characterCounter();
         //Assert
+        int expected = 0;
+        assertEquals(expected, actual);
     }
 }
