b3f7797f6d797b8a1dc086b7aebd01722416501f
FilipAndersson99 <andersson.filip.1999@gmail.com>
2021-10-29 15:39:24 +0200

    Main metoden möter nu kraven för del 2 av inlämningsuppgiften.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        TextEater text = new TextEater();
+        while (!text.stopTracker()) {
+            Scanner txt = new Scanner(System.in);
+            String myText = txt.nextLine();
+            text.textAdder(myText);
+        }
+        System.out.println(text.characterCounter()+" "+ text.rowCounter());
     }
 }
